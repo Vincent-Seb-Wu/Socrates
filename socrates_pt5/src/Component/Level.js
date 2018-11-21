c9990b1548697530ae28f514b3c211ea08597e56
Vincent Wu <vincentwu09@yahoo.com>
2018-11-21 11:04:32 +0800

    update

---
+++ socrates_pt5/src/Component/Level.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Level extends Component {
+    state = {  }
+    render() { 
+        return (<p></p>);
+    }
+}
+ 
+
+
+export default Level;
---
+++ socrates_pt5/src/Component/Next.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Next extends Component {
+    state = {  }
+    render() { 
+        return (<p></p>);
+    }
+}
+ 
+
+
+export default Next;
